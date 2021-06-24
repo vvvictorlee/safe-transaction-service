fa475223af7fa7fc4521d669b9a32122b6a9300f
李胜 <lisheng@lishengs-MacBook-Pro.local>
2021-06-24 14:30:28 +0800

    add hsctest

--- safe_transaction_service/history/management/commands/setup_service.py
+++ safe_transaction_service/history/management/commands/setup_service.py
@@ -127,6 +127,10 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ('0x3E5c63644E683549055b9Be8653de26E0B4CD36E', 13736914, '1.3.0+L2'),
         ('0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552', 13736914, '1.3.0'),
     ],
+    170: [
+        ('0xE51abdf814f8854941b9Fe8e3A4F65CAB4e7A4a8', 13714, '1.3.0+L2'),
+        ('0xb4A7C7da1631CF60A2Cf23ABc86986f99a1A7f70', 13714, '1.3.0'),
+    ],
 }
 
 PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
@@ -172,6 +176,9 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     EthereumNetwork.MUMBAI: [
         ('0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2', 13736914),  # v1.3.0
     ],
+    170: [
+        ('0xE89ce3bcD35bA068A9F9d906896D3d03Ad5C30EC', 13734),  # v1.3.0
+    ],
 }
 
 
@@ -192,7 +199,8 @@ class Command(BaseCommand):
 
         self.stdout.write(self.style.SUCCESS('Setting up Safe Contract Addresses'))
         ethereum_client = EthereumClientProvider()
-        ethereum_network = ethereum_client.get_network()
+        # ethereum_network = ethereum_client.get_network()
+        ethereum_network = ethereum_client.w3.net.version    
         if ethereum_network in MASTER_COPIES:
             self.stdout.write(self.style.SUCCESS(f'Setting up {ethereum_network.name} safe addresses'))
             self._setup_safe_master_copies(MASTER_COPIES[ethereum_network])
